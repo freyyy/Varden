894a771c32f46ee4335038ccdfcc374bfd93793d
vladgaman <science_vlad@yahoo.com>
2014-03-08 14:23:31 +0000

    third update

--- DotSurvival/Assets/DotMove.cs
+++ DotSurvival/Assets/DotMove.cs
@@ -3,51 +3,40 @@ using System.Collections;
 
 public class DotMove : MonoBehaviour {
 
-	public Vector2 dot;
 	public Camera cam;
 	Vector3 camCoord;
 	public float speed;
+	bool faceright;
 	// Use this for initialization
 	void Start () 
 	{
-		speed = 0.1f;
-		dot = transform.localPosition;
-		camCoord = transform.localPosition;
-		camCoord.z = -1.0f;
+		faceright = true;
+		speed = 5f;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
-		if (Input.GetKey (KeyCode.UpArrow)) 
-		{
-			dot.y += speed;
-			camCoord.y += speed;
-		}
-
-
-		if (Input.GetKey (KeyCode.DownArrow)) 
-		{
-			dot.y -= speed;
-			camCoord.y -= speed;
-		}
-
-
-		if (Input.GetKey (KeyCode.LeftArrow)) 
-		{
-			dot.x -= speed;
-			camCoord.x -= speed;
-		}
-
-
-		if (Input.GetKey (KeyCode.RightArrow)) 
-		{
-			dot.x += speed;
-			camCoord.x += speed;
-		}
-
+		float moveRL = Input.GetAxis ("Horizontal");
+		float moveUD = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector2 (moveRL * speed, moveUD * speed);
+		camCoord = transform.localPosition;
+		camCoord.z = -1.0f;
 		cam.transform.localPosition = camCoord;
 
-		transform.localPosition = dot;
+		if((moveRL > 0 && !faceright) || (moveRL < 0 && faceright))
+		{
+			Flip ();
+		}
+
 	}
+
+	void Flip()
+	{
+		faceright = !faceright;
+		Vector2 flip = transform.localScale;
+		flip.x *= -1;
+		transform.localScale = flip;
+	}
+
 }
